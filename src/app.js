c7f005fdaab5ea9b6cdb4ecc2d53f7c38db00148
gauravver13 <gauravver13@gmail.com>
2024-01-29 07:00:40 +0530

    Utils updated-Api- res, error, async

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import expree from 'express'
+import { cors } from "cors";
+import { cookieParser } from "cookie-parser";
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "32kb"}))
+app.use(express.urlencoded({extended: true, limit: "32kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+
+// MiddleWare
+
+export { app } 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
-
-
-
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
 
 
 
@@ -39,6 +45,8 @@ const app = express()
 
 // connectDB()
 
+
+//              Assignment: 1(listen event for error in app )
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/
