54e0af7d00391a02a16f9ca32e0a5e87c08b438e
gauravver13 <gauravver13@gmail.com>
2024-02-07 10:07:47 +0530

    added middleware and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,23 @@
+import multer from "multer"
+
+
+
+
+
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+
+      cb(null, file.originalname)
+    //   console.log(file);            // look at once!
+    }
+  })
+                                                                                                              
+  export const upload = multer({ 
+    storage,
+ })   
