8e76ddd5a030ccbe18cb78a1d8509479869bd409
gauravver13 <gauravver13@gmail.com>
2024-02-09 18:57:50 +0530

    fixes bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -191,58 +191,58 @@ const logoutUser = asyncHandler(async(req, res) => {
     .json(new ApiResponse(200, {}, "User logged out"))
 })
 
-const refreshAccessToken = asyncHandler(async(req, res) => {
-    //Algo:
-    // cookie refresh token 
+// const refreshAccessToken = asyncHandler(async(req, res) => {
+//     //Algo:
+//     // cookie refresh token 
 
-    const incommingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+//     const incommingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(!incommingRefreshToken) {
-        throw new ApiError(401, "Unauthorized request")
-    }
-    try {
+//     if(!incommingRefreshToken) {
+//         throw new ApiError(401, "Unauthorized request")
+//     }
+//     try {
         
-            const decodedToken = jwt.verify(
-                incommingRefreshToken,
-                process.env.REFRESH_TOKEN_SECRET
-            )
+//             const decodedToken = jwt.verify(
+//                 incommingRefreshToken,
+//                 process.env.REFRESH_TOKEN_SECRET
+//             )
         
-            const user = await User.findById(decodedToken?._id)
+//             const user = await User.findById(decodedToken?._id)
         
-            if(!user) {
-                throw new ApiError(401, "Invalid refresh token")
-            }
+//             if(!user) {
+//                 throw new ApiError(401, "Invalid refresh token")
+//             }
         
-            if(incommingRefreshToken !== user?.refreshToken) {
-                throw new ApiError(401, "Refresh token is expired or used")
-            }
+//             if(incommingRefreshToken !== user?.refreshToken) {
+//                 throw new ApiError(401, "Refresh token is expired or used")
+//             }
         
-            const options = {
-                httpOnly: true,
-                secure: true 
-            }
+//             const options = {
+//                 httpOnly: true,
+//                 secure: true 
+//             }
         
-            const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id)
+//             const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
         
-            return res
-            .status(200)
-            .cookie("accessToken", accessToken, options)
-            .cookie("refreshToken", newRefreshToken, options)
-            .json(
-                new ApiResponse(
-                    200,
-                    {accessToken, refreshToken},
-                    "Access token Successfully"
-                )
-            )   
-    } catch (error) {
-        throw new ApiError(401, error?.message || "")
-    }
-})
+//             return res
+//             .status(200)
+//             .cookie("accessToken", accessToken, options)
+//             .cookie("refreshToken", refreshToken, options)
+//             .json(
+//                 new ApiResponse(
+//                     200,
+//                     {accessToken, refreshToken},
+//                     "Access token Successfully"
+//                 )
+//             )   
+//     } catch (error) {
+//         throw new ApiError(401, error?.message || "")
+//     }
+// })
 
 export {
     registerUser,
     longinUser,
-    logoutUser,
-    refreshAccessToken
+    logoutUser
+    // refreshAccessToken
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -25,6 +25,7 @@ router.route("/login").post(longinUser)
 
 //Secured routes:
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+
+// router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
